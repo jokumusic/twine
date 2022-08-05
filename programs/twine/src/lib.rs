32de551e7e0176d22f7c57d2d56bb4d92638f1a6
joku <jokumusic@gmail.com>
2022-08-04 23:54:30 -0700

    initial commit

---
+++ programs/twine/src/lib.rs
@@ -0,0 +1,146 @@
+use anchor_lang::prelude::*;
+
+
+declare_id!("BKzDVQpoGW77U3ayBN6ELDbvEvSi2TYpSzHm8KhNmrCx");
+
+#[program]
+pub mod twine {
+    use super::*;
+
+    pub fn create_store(ctx: Context<CreateStore>, name: String, description: String) -> Result<()> {
+        let store = &mut ctx.accounts.store;
+        store.creator = ctx.accounts.payer.key();
+        store.owner = ctx.accounts.owner.key();
+        store.name = name;
+        store.description = description;
+        store.bump = *ctx.bumps.get("store").unwrap();
+
+        Ok(())
+    }
+
+    pub fn update_store(ctx: Context<UpdateStore>, name: String, description: String) -> Result<()> {
+        let store = &mut ctx.accounts.store;
+        store.name = name;
+        store.description = description;
+        Ok(())
+    }
+
+    pub fn change_store_owner(ctx: Context<UpdateStore>) -> Result<()> {
+        let store = &mut ctx.accounts.store;
+        store.owner = ctx.accounts.owner.key();
+        Ok(())
+    }
+
+}
+
+#[derive(Accounts)]
+pub struct CreateStore<'info> {
+    #[account(init, payer=payer, space=8+522, seeds=[b"store", owner.key.as_ref()], bump)]
+    pub store: Account<'info, Store>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    /// CHECK: this is is any account/pubkey that is authorized to modify the account. This is set by the payer on the initial creation
+    pub owner: UncheckedAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[derive(Accounts)]
+pub struct UpdateStore<'info> {
+    #[account(mut, has_one=owner, seeds=[b"store", owner.key.as_ref()], bump = store.bump)]   
+    pub store: Account<'info, Store>,
+
+    #[account()]
+    pub owner: Signer<'info>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ChangeStoreOwner<'info> {
+    #[account(mut, has_one=owner, seeds=[b"store", owner.key.as_ref()], bump = store.bump)]   
+    pub store: Account<'info, Store>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+}
+
+/*
+#[derive(Accounts)]
+pub struct CreateProduct<'info> {
+    #[account(init, payer=payer, space=8+1, seeds=[b"store", owner.key.as_ref()], bump)]
+    pub store: Account<'info, Store>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub owner: UncheckedAccount<'info>,
+    pub token_program: Program<'info, Token>,
+}
+*/
+
+#[account]
+pub struct Store{
+    pub bump: u8, //8; bump used for this PDA
+    pub creator: Pubkey, //32; original payer
+    pub owner: Pubkey, //32; current owner
+    pub name: String, //250; store name
+    pub description: String, //200; store description
+    //pub verified_by: Option<Pubkey>, //1 + 32; If verified, verified by who? store this outside this account
+    //pub rating: u8, //8; current rating; store this outside of the account
+}
+
+
+#[account]
+pub struct Product{
+    pub creator: Pubkey, //32; original transaction signer
+    pub owner: Pubkey, //32; address allowed to make changes
+    pub store: Pubkey, //32; address of store PDA
+    pub name: String, //250; product name
+    pub description: String, //200; product description
+    //pub category: u64, //64; bitwise AND masked identifier 
+    pub cost: u64, //64;
+    //pub verified_by: Option<Pubkey>, //1 + 8; store this outside of this account    
+    //pub rating: u8, //8; current rating; store this outside of the account
+}
+
+pub struct Purchase{
+    pub purchaser: Pubkey, //32; originally bought by
+    pub owner: Pubkey, //32; current owner
+    pub amount_paid: u64, //64; coins/tokens paid
+    pub max_uses: Option<u64>, //1+64; how many times the purchase can be used
+    pub expiration_date: u64, //64; minutes since 8/1/2022
+    pub uses: u64, //64;
+}
+
+
+/// Used as a bitwise mask for the product category
+/// this isn't a scalable way to store all the product categories - revisit this
+#[repr(u64)]
+pub enum ProductCategory{
+    None = 0,
+    Media = 1,
+    Merch = 2,
+    Event = 4,
+    Social = 8,
+}
+
+/// Used as a bitwise mask for the product type
+/// this isn't a scalable way to store all the product types - revisit this
+#[repr(u64)]
+pub enum ProductType {
+    None = 0,
+    Music = 1,
+    Video = 2,
+    Clothing = 4,
+    Concert = 8,
+    Conference = 16,
+    Award = 32,
+}
+
+
+
+
---
+++ tests/twine.ts
@@ -0,0 +1,77 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Twine } from "../target/types/twine";
+import {PublicKey, Keypair, sendAndConfirmTransaction} from "@solana/web3.js";
+import { assert, expect } from "chai";
+
+describe("twine", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const ownerKeypair = Keypair.generate();
+  const program = anchor.workspace.Twine as Program<Twine>;
+  const [storePda, storePdaBump] = PublicKey
+    .findProgramAddressSync([anchor.utils.bytes.utf8.encode("store"), ownerKeypair.publicKey.toBuffer()], program.programId);
+  const storeName = "test-store";
+  const storeDescription = "test-store description";
+
+
+  it("Create Store", async () => {
+    const tx = await program.methods
+    .createStore(storeName, storeDescription)
+    .accounts({
+      store: storePda,
+      payer: provider.publicKey,
+      owner: ownerKeypair.publicKey,
+    })
+    .rpc();
+
+    //console.log("Your transaction signature", tx);    
+
+    const createdStore = await program.account.store.fetch(storePda);
+    expect(createdStore.bump).is.equal(storePdaBump);
+    expect(createdStore.name).is.equal(storeName);
+    expect(createdStore.description).is.equal(storeDescription);
+    expect(createdStore.creator).is.eql(provider.publicKey);
+    expect(createdStore.owner).is.eql(ownerKeypair.publicKey);    
+    
+  });
+
+  it("Update Store", async () => {
+    const updatedStoreName = storeName + "-updated";
+    const updatedStoreDescription = storeDescription + "-updated";
+    
+    //this should fail because the owner is wrong
+    /*
+    const txfail = await program.methods
+    .updateStore(updatedStoreName, updatedStoreName)
+    .accounts({
+      store: storePda,
+      owner: ownerKeypair.publicKey,
+    })
+    .rpc();
+
+    //console.log("Your transaction signature", txfail);    
+
+    const updatedStoreFail = await program.account.store.fetch(storePda);
+    expect(updatedStoreFail.name).is.equal(storeName);
+    expect(updatedStoreFail.description).is.equal(storeDescription);
+  */
+
+    //this should succeed because the owner is correct
+    const txSuccess = await program.methods
+    .updateStore(updatedStoreName, updatedStoreDescription)
+    .accounts({
+      store: storePda,
+      owner: ownerKeypair.publicKey,
+      payer: provider.publicKey,
+    })
+    .transaction();
+
+    const txSucceeded = await provider.sendAndConfirm(txSuccess, [ownerKeypair]);
+    //const txSucceeded = await sendAndConfirmTransaction(program.provider.connection, txSuccess, [provider]);
+
+    const updatedStoreSuccess = await program.account.store.fetch(storePda);
+    expect(updatedStoreSuccess.name).is.equal(updatedStoreName);
+    expect(updatedStoreSuccess.description).is.equal(updatedStoreDescription);
+  });
+});
